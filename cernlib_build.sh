07f60d55cb7b49679f31aa81a1afcb8433d2f9ee
Mark Ito <marki@jlab.org>
2008-06-06 15:32:28 +0000

    use check in versions of patch files

--- cernlib_build.sh
+++ cernlib_build.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-#wget http://cernlib.web.cern.ch/cernlib/download/2006_source/tar/2006_src.tar.gz
-#wget http://cernlib.web.cern.ch/cernlib/download/2006_source/tar/include.tar.gz
+wget http://cernlib.web.cern.ch/cernlib/download/2006_source/tar/2006_src.tar.gz
+wget http://cernlib.web.cern.ch/cernlib/download/2006_source/tar/include.tar.gz
 tar zxf 2006_src.tar.gz
 tar zxf include.tar.gz
-cp ../cern/linux.cf $CERN_LEVEL/src/config/
-cp ../cern/iconwidget.c $CERN_LEVEL/src/packlib/kuip/code_motif/
+svn checkout $HDSVN/trunk/home/marki/cernlib_patches
 export CERN=`pwd`
 export CERN_LEVEL=2006
 export CERN_ROOT=$CERN/$CERN_LEVEL
 export CVSCOSRC=$CERN_ROOT/src
 export PATH=$CERN_ROOT/bin:$PATH
+cp cernlib_patches/linux.cf $CERN_LEVEL/src/config/
+cp cernlib_patches/iconwidget.c $CERN_LEVEL/src/packlib/kuip/code_motif/
 cd $CERN_ROOT
 mkdir -p build bin lib
 cd $CERN_ROOT/build
