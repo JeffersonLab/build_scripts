73bd6a6823847efc3090137ddf8ba9158af77596
Mark Ito <marki@jlab.org>
2009-11-25 14:57:29 +0000

    Make a definition for ifarml6

--- cue_cernlib.pl
+++ cue_cernlib.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 %cern = ('Linux_RHEL5-i686-gcc4.1.2' => 'i386_rhel5',
-	 'Linux_Fedora8-i686-gcc4.1.2' => 'i386_fc8'
+	 'Linux_Fedora8-i686-gcc4.1.2' => 'i386_fc8',,
+	 'Linux_CentOS5-x86_64-gcc4.1.2' => 'x86_64_rhel5'
 	 );
 $osrelease = `$ENV{BUILD_SCRIPTS}/osrelease.pl`;
 chomp $osrelease;
