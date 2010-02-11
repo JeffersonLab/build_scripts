38618934fd888ae93a7e293b23746befa7e688ec
Mark Ito <marki@jlab.org>
2010-02-11 18:57:06 +0000

    unsetenv LD_LIBRARY_PATH if there is nothing left

--- delpath.pl
+++ delpath.pl
@@ -13,22 +13,26 @@ $line=$ENV{$pathtype};
 @field = split(/:/,$line);
 #print "number of fields $#field\n";
 #print "number of fields to delete $#ARGV\n";
-$newpath = "";
-for ($j = 0; $j <= $#field ; $j++) {
-  $path = $field[$j];
-  #print "$j $path\n";
-  $keep = 1;
-  for($i = 0; $i <= $#ARGV; $i++) {
-    #print "$i $ARGV[$i]\n";
-    if ($path eq $ARGV[$i]) {
-      $keep = 0;
+if ($#field == 0) {
+    print "unsetenv $pathtype\n";
+} else {
+    $newpath = "";
+    for ($j = 0; $j <= $#field ; $j++) {
+	$path = $field[$j];
+	#print "$j $path\n";
+	$keep = 1;
+	for($i = 0; $i <= $#ARGV; $i++) {
+	    #print "$i $ARGV[$i]\n";
+	    if ($path eq $ARGV[$i]) {
+		$keep = 0;
+	    }
+	}
+	if ($keep) {
+	    #print "keep it\n";
+	    if ($newpath ne "") {$newpath = $newpath . ":"}
+	    $newpath = $newpath . $path;
+	}
     }
-  }
-  if ($keep) {
-    #print "keep it\n";
-    if ($newpath ne "") {$newpath = $newpath . ":"}
-    $newpath = $newpath . $path;
-  }
+    print "setenv $pathtype $newpath\n";
 }
-print "setenv $pathtype $newpath\n";
 exit
