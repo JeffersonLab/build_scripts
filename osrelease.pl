da705bf7a35b4f37940a360777abfc386a222462
Mark Ito <marki@jlab.org>
2011-01-03 22:15:07 +0000

    add Fedora 14

--- osrelease.pl
+++ osrelease.pl
@@ -29,7 +29,9 @@ chomp $uname;
 if ($uname eq 'Linux') {
     if (-e '/etc/fedora-release') {
 	$release_string = `cat /etc/fedora-release`;
-	if ($release_string =~ /^Fedora release 13.*/) {
+	if ($release_string =~ /^Fedora release 14.*/) {
+	    $release = '_Fedora14';
+	} elsif ($release_string =~ /^Fedora release 13.*/) {
 	    $release = '_Fedora13';
 	} elsif ($release_string =~ /^Fedora release 12.*/) {
 	    $release = '_Fedora12';
