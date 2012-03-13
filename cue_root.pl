30eb0b726be17ad7f0eff7541f94de736e714766
Mark Ito <marki@jlab.org>
2012-03-13 14:43:35 +0000

    another gcc version for rhel6

--- cue_root.pl
+++ cue_root.pl
@@ -3,7 +3,8 @@
 	 'Linux_Fedora8-i686-gcc4.1.2' => '/apps/root/PRO/root',
 	 'Linux_CentOS5-x86_64-gcc4.1.2' => '/apps/root/5.26-00-64bit/root',
 	 'Linux_RHEL5-x86_64-gcc4.1.2' => '/apps/root/5.26-00-64bit/root',
-	 'Linux_RHEL6-x86_64-gcc4.4.5' => '/apps/root/5.30.00/root'
+	 'Linux_RHEL6-x86_64-gcc4.4.5' => '/apps/root/5.30.00/root',
+	 'Linux_RHEL6-x86_64-gcc4.4.6' => '/apps/root/5.30.00/root'
 	    );
 $osrelease = `$ENV{BUILD_SCRIPTS}/osrelease.pl`;
 chomp $osrelease;
