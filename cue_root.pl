1d47f433ebf16fc371412c270d1bbdfdac668976
Mark Ito <marki@jlab.org>
2013-04-19 16:03:31 +0000

    define a version for Linux_RHEL6-x86_64-gcc4.4.7

--- cue_root.pl
+++ cue_root.pl
@@ -19,6 +19,7 @@
   'Linux_CentOS5-x86_64-gcc4.1.2' => '/group/halld/Software/ExternalPackages/ROOT/v5.34.01/root_Linux_CentOS5-x86_64-gcc4.1.2',
 
   'Linux_RHEL6-x86_64-gcc4.4.6'   => '/group/halld/Software/ExternalPackages/ROOT/v5.34.01/root_Linux_CentOS6-x86_64-gcc4.4.6',
+  'Linux_RHEL6-x86_64-gcc4.4.7'   => '/group/halld/Software/ExternalPackages/ROOT/v5.34.01/root_Linux_CentOS6-x86_64-gcc4.4.6',
   'Linux_CentOS6-x86_64-gcc4.4.6' => '/group/halld/Software/ExternalPackages/ROOT/v5.34.01/root_Linux_CentOS6-x86_64-gcc4.4.6',
   'Linux_CentOS6-x86_64-clang3.2' => '/group/halld/Software/ExternalPackages/ROOT/v5.34.01/root_Linux_CentOS6-x86_64-clang3.2'
  	    );
