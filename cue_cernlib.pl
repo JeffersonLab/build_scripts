2b76a69d8c90387f5e2b4075829c57ed0bbc0d41
Mark Ito <marki@jlab.org>
2013-04-23 17:07:40 +0000

    fix cernlib on RHEL6 on CUE

--- cue_cernlib.pl
+++ cue_cernlib.pl
@@ -7,15 +7,14 @@
 
   'Linux_RHEL5-x86_64-gcc4.1.2'   => '/group/halld/Software/ExternalPackages/cernlib/Linux_CentOS5-x86_64-gcc4.1.2',
   'Linux_CentOS5-x86_64-gcc4.1.2' => '/group/halld/Software/ExternalPackages/cernlib/Linux_CentOS5-x86_64-gcc4.1.2',
- 
-  'Linux_RHEL6-x86_64-gcc4.4.6'   => '/group/halld/Software/ExternalPackages/cernlib/Linux_CentOS6-x86_64-gcc4.4.6',
+   'Linux_RHEL6-x86_64-gcc4.4.6'   => '/group/halld/Software/ExternalPackages/cernlib/Linux_CentOS6-x86_64-gcc4.4.6',
+  'Linux_RHEL6-x86_64-gcc4.4.7'   => '/group/halld/Software/ExternalPackages/cernlib/Linux_CentOS6-x86_64-gcc4.4.6',
   'Linux_CentOS6-x86_64-gcc4.4.6' => '/group/halld/Software/ExternalPackages/cernlib/Linux_CentOS6-x86_64-gcc4.4.6',
   'Linux_CentOS6-x86_64-clang3.2' => '/group/halld/Software/ExternalPackages/cernlib/Linux_CentOS6-x86_64-gcc4.4.6'
  	 );
 $osrelease = `$ENV{BUILD_SCRIPTS}/osrelease.pl`;
 chomp $osrelease;
 #print "debug: osrelease = $osrelease\n";
-#print "debug: $cern{'Linux_RHEL5-i686-gcc4.1.2'}\n";
 $cern_cue = $cern{$osrelease};
 print "$cern_cue\n";
 exit;
