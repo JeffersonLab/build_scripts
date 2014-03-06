822a6ee89e1cdf9ca667bd1173dad5e617fad89f
Mark Ito <marki@jlab.org>
2014-03-06 14:24:40 +0000

    JANA_HOME change for bash

--- gluex_env.sh
+++ gluex_env.sh
@@ -91,7 +91,7 @@ if [ -z "$CCDB_HOME" ]; then export CCDB_HOME=$GLUEX_TOP/ccdb/prod; fi
 . $BUILD_SCRIPTS/ccdb_env.sh
 if [ -z "$CCDB_CONNECTION" ]; then export CCDB_CONNECTION=mysql://ccdb_user@hallddb.jlab.org/ccdb; fi
 # jana (JANA_GEOMETRY_URL depends on HDDS_HOME)
-if [ -z "$JANA_HOME" ]; then export JANA_HOME=$GLUEX_TOP/jana/prod; fi
+if [ -z "$JANA_HOME" ]; then export JANA_HOME=$GLUEX_TOP/jana/prod/$BMS_OSNAME; fi
 if [ -z "$JANA_CALIB_URL" ]
     then export JANA_CALIB_URL=$CCDB_CONNECTION
 fi
