6e9d41544cc7916be1eb46ea66fdd532ee7b8f3d
Mark Ito <marki@jlab.org>
2014-11-10 21:18:01 +0000

    works, warnings only, debug print out still in

--- version_check.pl
+++ version_check.pl
@@ -11,6 +11,7 @@ $build_scripts = dirname($this_file_with_full_path);
 $definitions = read_file("$build_scripts/version_defs.pl");
 eval $definitions;
 
+$version_hash = ();
 foreach $package (@packages) {
     print "== checking $package ==\n";
     $home_variable = $home_variable{$package};
@@ -18,6 +19,22 @@ foreach $package (@packages) {
     $home_value_hash{$package} = $home_value;
     if (-e $home_value) {
 	print "$home_variable = $home_value\n";
+	if ($package eq 'cernlib') {
+	    $version_hash{$package} = $ENV{CERN_LEVEL}
+	} else {
+	    # extract the version
+	    print "dir_prefix = $dir_prefix{$package}\n";
+	    @token0 = split(/$dir_prefix{$package}/, $home_value);
+	    $home_value_tail = $token0[1];
+	    print "dir_suffix = $dir_suffix{$package}\n";
+	    if ($dir_suffix{$package}) {
+		@token1 = split(/$dir_suffix{$package}/, $home_value_tail);
+		$version_hash{$package} = $token1[0];
+	    } else {
+		$version_hash{$package} = $token0[1];
+	    }
+	}
+	print "version = $version_hash{$package}\n";
     } else {
 	print "$home_variable not defined\n";
     }
@@ -25,11 +42,26 @@ foreach $package (@packages) {
 
 foreach $package (@packages) {
     if (-e $home_value_hash{$package}) {
-	print "look for prereqs file for $package\n";
+	print "== look for prereqs file for $package ==\n";
 	$home_value = $home_value_hash{$package};
 	$filename = $home_value . "/" . $package . "_prereqs_version.xml";
 	if (-e $filename) {
 	    print "found $filename\n";
+	    # slurp in the xml file
+	    $ref = XMLin($filename, KeyAttr => [], ForceArray => 1);
+	    $a = $ref->{'package'}; # an array reference;
+	    print "a = $a\n";
+	    @b = @{$a}; # an array
+	    print "b = @b\n";
+	    foreach $href (@b) {
+		$c = $href; # a hash reference
+		print "c = $c\n";
+		%d = %{$c}; # a hash
+		print "d{name} = $d{name}\n";
+		print "d{version} = $d{version}\n";
+		print "stored version = $version_hash{$d{name}}\n";
+		if ($version_hash{$d{name}} eq $d{version}) {print "versions match\n";} else {print "version mismatch\n";}
+	    }
 	} else {
 	    print "prerequisite version file $filename not found for $package\n";
 	}
@@ -37,74 +69,3 @@ foreach $package (@packages) {
 }
 
 exit;
-
-$filename = $ARGV[0];
-if (!$filename) {
-    die 'error: version file name must be supplied as first argument';
-}
-
-$ref = XMLin($filename, KeyAttr=>[]);
-$a = $ref->{'package'}; # an array reference;
-#print "a = $a\n";
-@b = @{$a}; # an array
-#print "b = @b\n";
-foreach $href (@b) {
-    $c = $href; # a hash reference
-    #print "c = $c\n";
-    %d = %{$c}; # a hash
-    #print "d{name} = $d{name}\n";
-    #print "d{version} = $d{version}\n";
-    $name = $d{name};
-    $name_in_caps = uc($name);
-    $name_in_caps =~ s/-/_/g;
-    $version = $d{version};
-    print_command("${name_in_caps}_VERSION", $version);
-    if ($name eq 'cernlib') {
-	print_command('CERN', "$gluex_top/cernlib");
-	print_command('CERN_LEVEL', $version);
-	print_command('CERNLIB_WORD_LENGTH', $d{word_length});
-    } else {
-	if ($version eq 'latest') {
-	    $package_home_dir = "$gluex_top/$name/$name$dir_suffix{$name}";
-	} else {
-	    $package_home_dir = "$gluex_top/$name/$dir_prefix{$name}$version$dir_suffix{$name}";
-	}
-	$package_home_var = $home_variable{$name};
-	print_command($package_home_var, $package_home_dir);
-    }
-}
-
-
-exit;
-
-sub define_shell_type {
-    $opt_s = "";
-    getopts('s:');
-    $shell = $opt_s;
-    my $shell_type = 'undefined';
-    if ($shell eq 'sh') {
-	$shell_type = 'Bourne';
-    } elsif ($shell eq 'bash') {
-	$shell_type = 'Bourne';
-    } elsif ($shell eq 'csh') {
-	$shell_type = 'C';
-    } elsif ($shell eq 'tcsh') {
-	$shell_type = 'C';
-    } elsif ($shell eq '') {
-	$shell_type = 'C';
-    } else {
-	die 'shell not recognized, use bash, sh, csh, or tcsh';
-    }
-    return $shell_type;
-}
-
-sub print_command {
-    my ($variable, $definition) = @_;
-    if ($shell_type eq 'C') {
-	print "setenv $variable $definition;\n";
-    } elsif ($shell_type eq 'Bourne') {
-	print "export ${variable}=${definition};\n";
-    } else {
-	die "unrecognized shell type: $shell_type";
-    }
-}
