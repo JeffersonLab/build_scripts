2e5e5be74434b10cab0821daf14f0e7791874eef
Mark Ito <marki@jlab.org>
2015-05-21 17:32:15 +0000

    add version based environment set-up

---
+++ gluex_env_version.csh
@@ -0,0 +1,29 @@
+#!/bin/tcsh
+
+set error_message="Error in gluex_env_version.csh"
+set error_action="environment settings not done"
+
+if (! -f $BUILD_SCRIPTS/version.pl) then
+    echo ${error_message}: $BUILD_SCRIPTS/version.pl not found, $error_action
+    exit 1
+endif
+if (! -f $BUILD_SCRIPTS/gluex_env.csh) then
+    echo ${error_message}: $BUILD_SCRIPTS/gluex_env.csh not found, $error_action
+    exit 4
+endif
+if ("x$1" != "x") then # version argument given
+    if (! -f $1) then
+        echo ${error_message}: version xml file $1 not found, $error_action
+        exit 2
+    else
+        set version_file=$1
+    endif
+else if (! -f "version.xml") then # look for version.xml in current working directory
+    echo ${error_message}: no argument given and no version.xml found in current working directory, $error_action
+    exit 3
+else
+    set version_file=version.xml
+endif
+
+eval `$BUILD_SCRIPTS/version.pl $version_file`
+source $BUILD_SCRIPTS/gluex_env.csh
---
+++ gluex_env_version.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+error_message="Error in gluex_env_version.sh"
+error_action="environment settings not done"
+
+if [ -z $BUILD_SCRIPTS ]
+    then
+    echo ${error_message}: BUILD_SCRIPTS not defined, $error_action
+    return 5
+fi
+if [ ! -f $BUILD_SCRIPTS/version.pl ]
+    then
+    echo ${error_message}: $BUILD_SCRIPTS/version.pl not found, $error_action
+    return 1
+fi
+if [ ! -f $BUILD_SCRIPTS/gluex_env.sh ]
+    then
+    echo ${error_message}: $BUILD_SCRIPTS/gluex_env.sh not found, $error_action
+    return 4
+fi
+if [ ! -z $1 ]
+    then # version argument given
+    if [ ! -f $1 ]
+	then
+        echo ${error_message}: version xml file $1 not found, $error_action
+        return 2
+    else
+        version_file=$1
+    fi
+elif [ ! -f "version.xml" ]
+    then # look for version.xml in current working directory
+    echo ${error_message}: no argument given and no version.xml found in current working directory, $error_action
+    return 3
+else
+    version_file=version.xml
+fi
+
+eval `$BUILD_SCRIPTS/version.pl -sbash $version_file`
+source $BUILD_SCRIPTS/gluex_env.sh
