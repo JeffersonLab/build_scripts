69a75c48a8eafa788bb056946cfde2f68b46d49c
Mark Ito <marki@jlab.org>
2015-05-28 17:40:16 +0000

    use version.xml for jlab set-up

--- gluex_env_jlab.csh
+++ gluex_env_jlab.csh
@@ -1,32 +1,10 @@
 #!/bin/tcsh
-#
-# only allow user variation in BUILD_SCRIPTS, JANA, HDDS, HALLD, HALLD_MY, and CCDB
-#
-if (! $?BUILD_SCRIPTS) setenv BUILD_SCRIPTS /group/halld/Software/scripts/build_scripts
+set VERSION_XML=/group/halld/www/halldweb/html/dist/version_1.5.xml
+setenv BUILD_SCRIPTS /group/halld/Software/scripts/build_scripts
 setenv BMS_OSNAME `$BUILD_SCRIPTS/osrelease.pl`
-# xerces-c
-setenv XERCESCROOT /group/halld/Software/builds/$BMS_OSNAME/xerces-c/xerces-c-3.1.1
-# Jana
-if (! $?JANA_HOME) setenv JANA_HOME /group/halld/Software/builds/$BMS_OSNAME/jana/jana_0.7.2/$BMS_OSNAME
-# ROOT
-setenv ROOTSYS /group/halld/Software/builds/$BMS_OSNAME/root/root_5.34.26
-# CERNLIB
-setenv CERN_CUE `$BUILD_SCRIPTS/cue_cernlib.pl`
-setenv CERN $CERN_CUE
-setenv CERN_LEVEL 2005
-# HDDS
-if (! $?HDDS_HOME) setenv HDDS_HOME /group/halld/Software/builds/$BMS_OSNAME/hdds/hdds-3.1
-# Hall D
-if (! $?HALLD_HOME) setenv HALLD_HOME /group/halld/Software/builds/$BMS_OSNAME/sim-recon/sim-recon-1.1.0
-if (! $?HALLD_MY) setenv HALLD_MY $HOME/halld_my
-# CCDB
-if (! $?CCDB_HOME) setenv CCDB_HOME /group/halld/Software/builds/$BMS_OSNAME/ccdb/ccdb_1.05
-# CLHEP
-setenv CLHEP /group/halld/Software/builds/$BMS_OSNAME/clhep/2.0.4.5/$BMS_OSNAME
-# EVIO
-setenv EVIOROOT /group/halld/Software/builds/$BMS_OSNAME/evio/evio-4.3.1/`uname -s`-`uname -m`
+setenv GLUEX_TOP /group/halld/Software/builds/$BMS_OSNAME
 # finish the rest of the gluex environment
-source $BUILD_SCRIPTS/gluex_env.csh
+source $BUILD_SCRIPTS/gluex_env_version.csh $VERSION_XML
 setenv JANA_CALIB_URL $CCDB_CONNECTION
 # python on the cue
 setenv PATH /apps/python/PRO/bin:$PATH
--- gluex_env_jlab.sh
+++ gluex_env_jlab.sh
@@ -1,32 +1,10 @@
 #!/bin/tcsh
-#
-# only allow user variation in BUILD_SCRIPTS, JANA, HDDS, HALLD, HALLD_MY, and CCDB
-#
-if [ -z "$BUILD_SCRIPTS" ]; then export BUILD_SCRIPTS=/group/halld/Software/scripts/build_scripts ; fi
+VERSION_XML=/group/halld/www/halldweb/html/dist/version_1.5.xml
+export BUILD_SCRIPTS=/group/halld/Software/scripts/build_scripts
 export BMS_OSNAME=`$BUILD_SCRIPTS/osrelease.pl`
-# xerces-c
-export XERCESCROOT=/group/halld/Software/builds/$BMS_OSNAME/xerces-c/xerces-c-3.1.1
-# Jana
-if [ -z "$JANA_HOME" ]; then export JANA_HOME=/group/halld/Software/builds/$BMS_OSNAME/jana/jana_0.7.2/$BMS_OSNAME; fi
-# ROOT
-export ROOTSYS=/group/halld/Software/builds/$BMS_OSNAME/root/root_5.34.26
-# CERNLIB
-export CERN_CUE=`$BUILD_SCRIPTS/cue_cernlib.pl`
-export CERN=$CERN_CUE
-export CERN_LEVEL=2005
-# HDDS
-if [ -z "$HDDS_HOME" ]; then export HDDS_HOME=/group/halld/Software/builds/$BMS_OSNAME/hdds/hdds-3.1 ; fi
-# Hall D
-if [ -z "$HALLD_HOME" ]; then export HALLD_HOME=/group/halld/Software/builds/$BMS_OSNAME/sim-recon/sim-recon-1.1.0 ; fi
-if [ -z "$HALLD_MY" ]; then export HALLD_MY=$HOME/halld_my; fi
-# CCDB
-if [ -z "$CCDB_HOME" ]; then export CCDB_HOME=/group/halld/Software/builds/$BMS_OSNAME/ccdb/ccdb_1.05; fi
-# CLHEP
-export CLHEP=/group/halld/Software/builds/$BMS_OSNAME/clhep/2.0.4.5/$BMS_OSNAME
-# EVIO
-export EVIOROOT=/group/halld/Software/builds/$BMS_OSNAME/evio/evio-4.3.1/`uname -s`-`uname -m`
+export GLUEX_TOP=/group/halld/Software/builds/$BMS_OSNAME
 # finish the rest of the environment
-. $BUILD_SCRIPTS/gluex_env.sh
+. $BUILD_SCRIPTS/gluex_env_version.sh $VERSION_XML
 export JANA_CALIB_URL=$CCDB_CONNECTION
 # python on the cue
 export PATH=/apps/python/PRO/bin:$PATH
