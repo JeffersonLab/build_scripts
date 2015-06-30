61d11c943a95e6fe7cfe832b7e9b420482c7abe5
Mark Ito <marki@jlab.org>
2015-06-30 17:59:09 +0000

    go to version_1.6.xml

--- gluex_env_jlab.sh
+++ gluex_env_jlab.sh
@@ -1,5 +1,5 @@
 #!/bin/tcsh
-VERSION_XML=/group/halld/www/halldweb/html/dist/version_1.5.xml
+VERSION_XML=/group/halld/www/halldweb/html/dist/version_1.6.xml
 export BUILD_SCRIPTS=/group/halld/Software/scripts/build_scripts
 export BMS_OSNAME=`$BUILD_SCRIPTS/osrelease.pl`
 export GLUEX_TOP=/group/halld/Software/builds/$BMS_OSNAME
