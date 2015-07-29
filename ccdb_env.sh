a65e249a0202107194c6829132b188466a8df373
Mark Ito <marki@jlab.org>
2015-07-29 18:39:46 +0000

    fix pythonpath for bash when previous definition exists

--- ccdb_env.sh
+++ ccdb_env.sh
@@ -5,7 +5,7 @@ if [ `echo $LD_LIBRARY_PATH | grep -c $CCDB_HOME/lib` -eq 0 ]
 fi
 if [ -z "$PYTHONPATH" ]; then export PYTHONPATH=''; fi
 if [ `echo $PYTHONPATH | grep -c $CCDB_HOME/python` -eq 0 ]
-    then export PYTHONPATH=$CCDB_HOME/python:$CCDB_HOME/python/ccdb/ccdb_pyllapi$PYTHONPATH
+    then export PYTHONPATH=$CCDB_HOME/python:$CCDB_HOME/python/ccdb/ccdb_pyllapi:$PYTHONPATH
 fi
 if [ `echo $PATH | grep -c $CCDB_HOME/bin` -eq 0 ]
     then export PATH=$CCDB_HOME/bin:$PATH
