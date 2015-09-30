945ff5850015111a2984e22f1401d4fe3ecb2427
Sean Dobbs <seandobbs@gmail.com>
2015-09-30 00:10:02 -0400

    Script to leave a comment on a GitHub issue - inputs: URL of API location to leave comment, text of comment - GitHub user/pass combo should be stored in a file, the location of which is passed as a comment
    Note that this script relies on the 'requests' python library and python3

---
+++ leave_pull_request_comment.py
@@ -0,0 +1,37 @@
+#!/apps/python/3.3.1/bin/python3
+
+# note that this is in python 3, since the version of python 2.7 on the CUE is not
+# new enough to properly support the libraries that handle our SSL connection to GitHub
+# python 3 has many tricky difference between python2 - be warned!
+
+import requests
+import sys
+
+if len(sys.argv) < 4:
+    print("Not enough arguments!")
+    exit
+
+user = None
+password = None
+
+# github credentials are stored in separate file
+login_file = sys.argv[1]
+with open(login_file) as f:
+    lines = f.readlines()
+    user = lines[0].strip()
+    password = lines[1].strip()
+
+if user is None or password is None:
+    print("invalid credentials")
+    exit
+
+comment_url = sys.argv[2]
+comment = sys.argv[3]
+
+# leave comment
+message = "{ \"body\" : \""+comment+"\" }"
+print (message)
+r = requests.post(comment_url, data=message, auth=(user,password))
+# response stored as "r.text"
+print("Result of comment POST:")
+print(r.text)
