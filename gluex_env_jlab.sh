4f121f1f948d289e045ee5b150523ecf3455f958
Mark Ito <marki@jlab.org>
2016-10-14 16:35:36 -0400

    Fixes after testing.

--- gluex_env_jlab.sh
+++ gluex_env_jlab.sh
@@ -13,11 +13,12 @@ if [[ $nodename =~ ^farm* || $nodename =~ ^ifarm* || $nodename =~ ^qcd* || $node
     then
     export http_proxy=http://jprox.jlab.org:8081
     export https_proxy=https://jprox.jlab.org:8081
-    if [[ $BMS_OSNAME =~ *CentOS6* || $BMS_OSNAME =~ *RHEL6* ]]
+    if [[ $BMS_OSNAME == *CentOS6* || $BMS_OSNAME == *RHEL6* ]]
 	then
 	GCC_HOME=/apps/gcc/4.9.2
 	export PATH=${GCC_HOME}/bin:${PATH}
 	export LD_LIBRARY_PATH=${GCC_HOME}/lib64:${GCC_HOME}/lib
+	export BMS_OSNAME=`$BUILD_SCRIPTS/osrelease.pl`
     fi
 fi
 export GLUEX_TOP=/group/halld/Software/builds/$BMS_OSNAME
