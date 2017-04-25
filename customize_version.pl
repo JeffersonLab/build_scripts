333f18462ae9290d2982153fad825a0297736782
GlueX-Hall-D (Mark Ito) <gluex@jlabl5.jlab.org>
2017-04-25 17:08:49 -0400

    Allow customization of hdgeant4 and gluex_root_analysis.

--- customize_version.pl
+++ customize_version.pl
@@ -8,11 +8,13 @@ use XML::Writer;
 use IO::File;
 
 # get the file name
-getopts("i:o:s:g:h");
+getopts("i:o:s:g:m:a:h");
 $filename_in = $opt_i;
 $filename_out = $opt_o;
 $halld_home = $opt_s;
 $hdds_home = $opt_g;
+$hdgeant4_home = $opt_m;
+$root_analysis_home = $opt_a;
 
 if ($opt_h) {
     print_usage();
@@ -23,7 +25,7 @@ if (!$filename_in || !$filename_out ) {
     print_usage();
     exit 1;
 }
-if (!($halld_home || $hdds_home)) {
+if (!($halld_home || $hdds_home || $hdgeant4_home || $root_analysis_home)) {
     print "\nError: no custom home directories specified, no action taken\n\n";
     print_usage();
     exit 2;
@@ -56,9 +58,21 @@ foreach $href (@b) {
     $hash = $d{hash};
     #print "package = $name, version = $version\n";
     if ($name eq "sim-recon" && $halld_home) {
-	$writer->emptyTag("package", "name" => "$name", "home" => "$halld_home");
+	if (uc($halld_home) ne "NONE") {
+	    $writer->emptyTag("package", "name" => "$name", "home" => "$halld_home");
+	}
     } elsif ($name eq "hdds" && $hdds_home) {
-	$writer->emptyTag("package", "name" => "$name", "home" => "$hdds_home");
+	if (uc($hdds_home) ne "NONE") {
+	    $writer->emptyTag("package", "name" => "$name", "home" => "$hdds_home");
+	}
+    } elsif ($name eq "hdgeant4" && $hdgeant4_home) {
+	if (uc($hdgeant4_home) ne "NONE") {
+	    $writer->emptyTag("package", "name" => "$name", "home" => "$hdgeant4_home");
+	}
+    } elsif ($name eq "gluex_root_analysis" && $root_analysis_home) {
+	if (uc($root_analysis_home) ne "NONE") {
+	    $writer->emptyTag("package", "name" => "$name", "home" => "$root_analysis_home");
+	}
     } else {
 	$write_element_command = "\$writer->emptyTag(\"package\", \"name\" => \"$name\"";
 	if ($version) {
@@ -102,6 +116,8 @@ Options:
     -o <output xml file name> (required)
     -s <custom sim-recon home directory> (optional, s for sim-recon)
     -g <custom HDDS home directory> (optional, g for geometry)
+    -m <custom HDGeant4 home directory> (optional, m for Monte Carlo)
+    -a <custom gluex_root_analysis home directory> (optional, a for analysis)
     -h print this usage message
 EOM
 
