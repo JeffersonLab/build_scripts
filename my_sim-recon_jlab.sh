e0334507b4e3e093223eb03bf42428c257e97ff4
markito3 <marki@jlab.org>
2017-04-26 09:30:41 -0400

    Make custom versions of hdgeant4 and gluex_root_analysis as well since those depend on the custom sim-recon.

--- my_sim-recon_jlab.sh
+++ my_sim-recon_jlab.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 export BUILD_SCRIPTS=/group/halld/Software/build_scripts
-$BUILD_SCRIPTS/customize_version.pl -i /group/halld/www/halldweb/html/dist/version_jlab.xml -o ${USER}_`date +%F`.xml -s `pwd`/sim-recon
-echo source $BUILD_SCRIPTS/gluex_env_jlab.sh `pwd`/${USER}_`date +%F`.xml > setup_gluex.sh
-echo source $BUILD_SCRIPTS/gluex_env_jlab.csh `pwd`/${USER}_`date +%F`.xml > setup_gluex.csh
+$BUILD_SCRIPTS/customize_version.pl \
+    -i /group/halld/www/halldweb/html/dist/version_jlab.xml \
+    -o ${USER}_`date +%F`.xml \
+    -s `pwd`/sim-recon \
+    -m `pwd`/hdgeant4 \
+    -a `pwd`/gluex_root_analysis
+echo source $BUILD_SCRIPTS/gluex_env_jlab.sh `pwd`/${USER}_`date +%F`.xml \
+    > setup_gluex.sh
+echo source $BUILD_SCRIPTS/gluex_env_jlab.csh `pwd`/${USER}_`date +%F`.xml \
+    > setup_gluex.csh
 source $BUILD_SCRIPTS/gluex_env_jlab.sh `pwd`/${USER}_`date +%F`.xml
-make -f $BUILD_SCRIPTS/Makefile_sim-recon SIM_RECON_SCONS_OPTIONS="-j8"
+make -f $BUILD_SCRIPTS/Makefile_sim-recon SIM_RECON_SCONS_OPTIONS="-j4"
+make -f $BUILD_SCRIPTS/Makefile_hdgeant4
+make -f $BUILD_SCRIPTS/Makefile_gluex_root_analysis
