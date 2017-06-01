31f7b39c3d7e104da000d1a16bfa6d3eac5b197b
Matthew Shepherd <mashephe@indiana.edu>
2017-06-01 16:14:28 -0400

    added support for osx10.12

--- osrelease.pl
+++ osrelease.pl
@@ -108,6 +108,8 @@ if ($uname eq 'Linux') {
         $release = '_macosx10.10';
     } elsif ($release_string =~ /^15.*/) {
         $release = '_macosx10.11';
+    } elsif ($release_string =~ /^16.*/) {
+        $release = '_macosx10.12';
 	} else {
 	    print STDERR "unrecognized Mac OS X (Darwin) release\n";
 	    $release = '_macosx';
