f06287a671cf674981e6218a5fa09f281d43a3c9
markito3 <marki@jlab.org>
2017-06-21 12:48:08 -0400

    Add custom home directories for gluex_root_analysis and hdgeant4. Use these options in the nightly build.

--- customize_version.pl
+++ customize_version.pl
@@ -8,11 +8,13 @@ use XML::Writer;
 use IO::File;
 
 # get the file name
-getopts("i:o:s:g:h");
+getopts("i:o:s:g:4:a:h");
 $filename_in = $opt_i;
 $filename_out = $opt_o;
 $halld_home = $opt_s;
 $hdds_home = $opt_g;
+$hdgeant4_home = $opt_4;
+$gluex_root_analysis_home = $opt_a;
 
 if ($opt_h) {
     print_usage();
@@ -23,7 +25,7 @@ if (!$filename_in || !$filename_out ) {
     print_usage();
     exit 1;
 }
-if (!($halld_home || $hdds_home)) {
+if (!($halld_home || $hdds_home || $hdgeant4_home || $gluex_root_analysis_home)) {
     print "\nError: no custom home directories specified, no action taken\n\n";
     print_usage();
     exit 2;
@@ -59,6 +61,10 @@ foreach $href (@b) {
 	$writer->emptyTag("package", "name" => "$name", "home" => "$halld_home");
     } elsif ($name eq "hdds" && $hdds_home) {
 	$writer->emptyTag("package", "name" => "$name", "home" => "$hdds_home");
+    } elsif ($name eq "hdgeant4" && $hdgeant4_home) {
+	$writer->emptyTag("package", "name" => "$name", "home" => "$hdgeant4_home");
+    } elsif ($name eq "gluex_root_analysis" && $gluex_root_analysis_home) {
+	$writer->emptyTag("package", "name" => "$name", "home" => "$gluex_root_analysis_home");
     } else {
 	$write_element_command = "\$writer->emptyTag(\"package\", \"name\" => \"$name\"";
 	if ($version) {
@@ -102,6 +108,8 @@ Options:
     -o <output xml file name> (required)
     -s <custom sim-recon home directory> (optional, s for sim-recon)
     -g <custom HDDS home directory> (optional, g for geometry)
+    -4 <custom HDGeant4 home directory> (optional, 4 for 4)
+    -a <custom gluex_root_analysis home directory> (optional, a for analysis)
     -h print this usage message
 EOM
 
