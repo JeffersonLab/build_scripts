ba34d3c28b7d16a5704e07eccfd4cde9a18ed082
markito3 <marki@jlab.org>
2017-06-23 17:45:47 -0400

    Do custom hdds as well.

--- my_sim-recon_jlab.sh
+++ my_sim-recon_jlab.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-export BUILD_SCRIPTS=/group/halld/Software/build_scripts
+if [ -z "$BUILD_SCRIPTS" ]
+    then export BUILD_SCRIPTS=/group/halld/Software/build_scripts
+fi
+mydir=`pwd`
+mydate=`date +%F`
 $BUILD_SCRIPTS/customize_version.pl \
     -i /group/halld/www/halldweb/html/dist/version_jlab.xml \
-    -o ${USER}_`date +%F`.xml \
-    -s `pwd`/sim-recon \
-    -m `pwd`/hdgeant4 \
-    -a `pwd`/gluex_root_analysis
-echo source $BUILD_SCRIPTS/gluex_env_jlab.sh `pwd`/${USER}_`date +%F`.xml \
+    -o ${USER}_$mydate.xml \
+    -g $mydir/hdds \
+    -s $mydir/sim-recon \
+    -4 $mydir/hdgeant4 \
+    -a $mydir/gluex_root_analysis
+echo source $BUILD_SCRIPTS/gluex_env_jlab.sh $mydir/${USER}_$mydate.xml \
     > setup_gluex.sh
-echo source $BUILD_SCRIPTS/gluex_env_jlab.csh `pwd`/${USER}_`date +%F`.xml \
+echo source $BUILD_SCRIPTS/gluex_env_jlab.csh $mydir/${USER}_$mydate.xml \
     > setup_gluex.csh
-source $BUILD_SCRIPTS/gluex_env_jlab.sh `pwd`/${USER}_`date +%F`.xml
+source $BUILD_SCRIPTS/gluex_env_jlab.sh $mydir/${USER}_$mydate.xml
+set -e
+make -f $BUILD_SCRIPTS/Makefile_hdds
 make -f $BUILD_SCRIPTS/Makefile_sim-recon SIM_RECON_SCONS_OPTIONS="-j4"
 make -f $BUILD_SCRIPTS/Makefile_hdgeant4
 make -f $BUILD_SCRIPTS/Makefile_gluex_root_analysis
