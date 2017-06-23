5e8a6403de6d693709b0ef90413646897b6a10e3
markito3 <marki@jlab.org>
2017-06-23 14:07:27 -0400

    Use absolute paths for gluex_env_jlab.(c)sh.

--- gluex_env_nightly.sh
+++ gluex_env_nightly.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 BUILD_DATE=$1
 export BMS_OSNAME=`/group/halld/Software/build_scripts/osrelease.pl`
-source gluex_env_jlab.sh /u/scratch/gluex/nightly/$BUILD_DATE/$BMS_OSNAME/version_$BUILD_DATE.xml
+source /group/halld/Software/build_scripts/gluex_env_jlab.sh /u/scratch/gluex/nightly/$BUILD_DATE/$BMS_OSNAME/version_$BUILD_DATE.xml
