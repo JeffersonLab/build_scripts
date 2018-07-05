791e3e9050598304c9ed8617ed50567a07b20dab
markito3 <marki@jlab.org>
2018-07-04 20:42:21 -0400

    fixes to Makefile_all and version prerequisites

--- version_prereqs.pl
+++ version_prereqs.pl
@@ -35,12 +35,19 @@ eval $definitions;
 	    'xerces-c' => [],
 	    geant4 => [],
 	    ccdb => [],
-	    hdgeant4 => ['geant4', 'sim-recon', 'jana', 'ccdb'],
-	    gluex_root_analysis => ['sim-recon', 'root'],
 	    amptools => ['root']);
 # add a prerequisite for sqlitecpp only if SQLITE_HOME is defined
 if (defined $ENV{SQLITE_HOME}) {
     $prereqs{sqlitecpp} = ['sqlite'];
+}
+# set prerequisites for hdgeant4 and gluex_root_analysis according to whether
+# HALLD_HOME is set
+if (defined $ENV{HALLD_HOME}) {
+    $prereqs{hdgeant4} = ['geant4', 'sim-recon', 'jana', 'ccdb'];
+    $prereqs{gluex_root_analysis} = ['sim-recon', 'root'];
+} else {
+    $prereqs{hdgeant4} = ['geant4', 'halld_recon', 'jana', 'ccdb'];
+    $prereqs{gluex_root_analysis} = ['halld_recon', 'root'];
 }
 
 @prepackages = @{$prereqs{$package_in}};
