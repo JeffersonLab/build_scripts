3aa828ad115b49a1a8a8106eb7bfdaf0a2728ae2
markito3 <marki@jlab.org>
2019-12-11 13:11:30 -0500

    first draft of test on merge script

---
+++ merge_test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#cd /u/scratch/gluex
+cd /local/scratch
+rm -rfv merge_test
+mkdir merge_test
+cd merge_test
+echo PWD = `pwd`
+git clone https://github.com/jeffersonlab/build_scripts
+export BUILD_SCRIPTS=`pwd`/build_scripts
+source $BUILD_SCRIPTS/gluex_env_boot_jlab.sh
+gxenv
+if which hd_root
+    then
+    echo success
+else
+    echo failure
+    exit 1
+fi
+if which mcsmear
+    then
+    echo success
+else
+    echo failure
+    exit 1
+fi
+if which hdgeant4
+    then
+    echo success
+else
+    echo failure
+    exit 1
+fi
+exit 0
