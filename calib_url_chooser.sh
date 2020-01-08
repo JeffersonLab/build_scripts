917cee6fbaec44c566a2f5bb5ba518db920f22d3
markito3 <marki@jlab.org>
2020-01-08 13:25:25 -0500

    Go back to multiple sqlite files on work disk.

--- calib_url_chooser.sh
+++ calib_url_chooser.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 # farm-specific set-up
-sqliteflag=$1
-if [ "$sqliteflag" == "sqlite" ]
-then
+nodename=`uname -n`
+if [[ $nodename =~ ^farm* || $nodename =~ ^qcd* ]]
+    then
     echo sqlite:////work/halld/ccdb_sqlite/$((1 + RANDOM % 100))/ccdb.sqlite
 else
-    nodename=`uname -n`
-    if [[ $nodename =~ ^farm* || $nodename =~ ^qcd* ]]
-    then
-	echo mysql://ccdb_user@hallddb-farm.jlab.org/ccdb
-    else
-	echo mysql://ccdb_user@hallddb.jlab.org/ccdb
-    fi
+    echo mysql://ccdb_user@hallddb.jlab.org/ccdb
 fi
