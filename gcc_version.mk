056a3df156809290cdae24455a1af749dda7e3ba
markito3 <marki@jlab.org>
2020-07-23 17:47:53 -0400

    intermediate check-in, changes to get fedora32 build to work on RHEL7

---
+++ gcc_version.mk
@@ -0,0 +1,7 @@
+GCC_VERSION := $(shell gcc --version | head -1 | awk '{print $$3}')
+GCC_MAJOR_VERSION := $(shell echo $(GCC_VERSION) | awk -F. '{print $$1}')
+GCC_MINOR_VERSION := $(shell echo $(GCC_VERSION) | awk -F. '{print $$2}')
+GCC_SUBMINOR_VERSION := $(shell echo $(GCC_VERSION) | awk -F. '{print $$3}')
+GCC_GE_9 := $(shell if [ $(GCC_MAJOR_VERSION) -ge "9" ]; then echo true; else echo false; fi)
+GCC_GE_9 := $(shell if [ $(GCC_MAJOR_VERSION) -ge "9" ]; then echo true; else echo false; fi)
+GCC_GE_10 := $(shell if [ $(GCC_MAJOR_VERSION) -ge "10" ]; then echo true; else echo false; fi)
