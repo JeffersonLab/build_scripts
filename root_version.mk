0ae200b0013ef5514157af0c83d19a6c8ab3b8a9
Mark Ito <markito3@gmail.com>
2021-10-07 19:31:09 -0400

    patch root cmake helper file

--- root_version.mk
+++ root_version.mk
@@ -3,4 +3,5 @@ ifdef ROOT_VERSION
     ROOT_MINOR_VERSION := $(shell echo $(ROOT_VERSION) | awk -F. '{print $$2}' | bc)
     ROOT_SUBMINOR_VERSION := $(shell echo $(ROOT_VERSION) | awk -F. '{print $$3}' | bc)
     ROOT_LE_6_08 := $(shell if [[ $(ROOT_MAJOR_VERSION) -lt 6 || $(ROOT_MAJOR_VERSION) -eq 6 && $(ROOT_MINOR_VERSION) -le 8 ]]; then echo true; else echo false; fi)
+    ROOT_EQ_6_24 := $(shell if [[ $(ROOT_MAJOR_VERSION) -eq 6 && $(ROOT_MINOR_VERSION) -eq 24 ]]; then echo true; else echo false; fi)
 endif
