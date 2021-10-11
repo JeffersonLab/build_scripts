f16bf788235c9ba0c8f4de92842bbd9fb7459f1a
Mark Ito <markito3@gmail.com>
2021-10-10 21:36:37 -0400

    syntax clean-up

--- gluex_env_boot_jlab.sh
+++ gluex_env_boot_jlab.sh
@@ -6,14 +6,16 @@ else
 fi
 export HALLD_VERSIONS=/group/halld/www/halldweb/html/halld_versions
 unset gxclean
-function gxclean() { source $BUILD_SCRIPTS/gluex_env_clean.sh; }
+function gxclean {
+    source $BUILD_SCRIPTS/gluex_env_clean.sh
+}
 unset gxclean_all
-function gxclean_all() {
+function gxclean_all  {
     source $BUILD_SCRIPTS/gluex_env_clean.sh
     unset BUILD_SCRIPTS HALLD_VERSIONS gxclean gxclean_all gxenv
 }
 unset gxenv
-function gxenv() {
+function gxenv {
     gxclean
     source $BUILD_SCRIPTS/gluex_env_jlab.sh $1
 }
