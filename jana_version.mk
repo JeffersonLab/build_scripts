ec6251982f6220497970ebd23f6448855d477abf
Alex Austregesilo <aaustreg@jlab.org>
2024-08-12 17:52:10 -0400

    this version builds jana 2.1.2

---
+++ jana_version.mk
@@ -0,0 +1,7 @@
+ifdef JANA_VERSION
+    JANA_MAJOR_VERSION := $(shell echo $(JANA_VERSION) | awk -F. '{print $$1}')
+    JANA_MINOR_VERSION := $(shell echo $(JANA_VERSION) | awk -F. '{print $$2}' | bc)
+    JANA_SUBMINOR_VERSION := $(shell echo $(JANA_VERSION) | awk -F. '{print $$3}' | bc)
+else
+     JANA_MAJOR_VERSION := 0
+endif
