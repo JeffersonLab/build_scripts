0336cdad16de9bc99d3e43c631113cb481624074
Alex Austregesilo <aaustreg@jlab.org>
2024-08-13 13:59:44 -0400

    few more edits for custom versions

--- jana_version.mk
+++ jana_version.mk
@@ -3,5 +3,5 @@ ifdef JANA_VERSION
     JANA_MINOR_VERSION := $(shell echo $(JANA_VERSION) | awk -F. '{print $$2}' | bc)
     JANA_SUBMINOR_VERSION := $(shell echo $(JANA_VERSION) | awk -F. '{print $$3}' | bc)
 else
-     JANA_MAJOR_VERSION := 0
+     JANA_MAJOR_VERSION := 2
 endif
