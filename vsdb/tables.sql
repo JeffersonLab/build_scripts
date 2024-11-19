3bccdc7a2bc27fa290a4a12e020890ba8c2cd39b
tbritton <tbritton@jlab.org>
2024-11-19 13:26:21 -0500

    add OSName to the versionSet table for use in mcwrapper and beyond

--- vsdb/tables.sql
+++ vsdb/tables.sql
@@ -59,6 +59,7 @@ CREATE TABLE `version` (
   `word_length` varchar(64) DEFAULT NULL,
   `debug_level` int(11) DEFAULT NULL,
   `onOasis` tinyint(1) DEFAULT 0,
+  `OS_ID` int DEFAULT 1,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
